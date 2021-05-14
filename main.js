9aeb9fe988e96ddeefe79610008a929f7029249e
Fabio Flores <efabiofm@gmail.com>
2021-05-14 00:09:05 -0600

    agregar puntos y funcion de reiniciar

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
  */
 const TABLERO = document.getElementById('tablero');
 const TABLERO_CTX = TABLERO.getContext('2d');
+const REINICIAR_BTN = document.getElementById('reiniciar');
+const PUNTAJE_TXT = document.getElementById('puntaje');
 
 /**
  * Controles
@@ -16,19 +18,22 @@ const DOWN_KEY = 40;
 /**
  * Variables
  */
-let dx = 10;
-let dy = 0;
-let timeout = 100;
-let comida;
+let dx, dy, timeout, comida, puntaje, culebra;
 
-/**
- * Culebra
- */
-let culebra = [
-  { x: 100, y: 100 }, // Cabeza
-  { x: 90, y: 100 },
-  { x: 80, y: 100 }
-];
+function inicializar() {
+  dx = 10;
+  dy = 0;
+  timeout = 100;
+  comida;
+  puntaje = 0;
+  culebra = [
+    { x: 50, y: 100 }, // Cabeza
+    { x: 40, y: 100 },
+    { x: 30, y: 100 }
+  ];
+  crearComida();
+  main();
+}
 
 function limpiarTablero() {
   TABLERO_CTX.fillStyle = 'black';
@@ -42,6 +47,17 @@ function dibujarCulebra() {
   }
 }
 
+function incrementarPuntos() {
+  puntaje += 10;
+  PUNTAJE_TXT.innerText = puntaje;
+}
+
+function aumentarVelocidad() {
+  if (timeout > 0) {
+    timeout -= 2;
+  }
+}
+
 function moverCulebra() {
   const CABEZA = {
     x: culebra[0].x + dx,
@@ -52,6 +68,8 @@ function moverCulebra() {
   // Si la cabeza y la comida estan en la misma posición
   // significa que comió y se crea una comida nueva
   if (CABEZA.x === comida.x && CABEZA.y === comida.y) {
+    incrementarPuntos();
+    aumentarVelocidad();
     crearComida();
   } else {
     // Quitar el último cuadrado del cuerpo si no ha comido
@@ -120,12 +138,29 @@ function crearComida() {
 }
 
 function dibujarComida() {
-  TABLERO_CTX.fillStyle = 'red';
+  TABLERO_CTX.fillStyle = 'yellow';
   TABLERO_CTX.fillRect(comida.x, comida.y, 10, 10);
 }
 
+function terminarJuego() {
+  limpiarTablero();
+  REINICIAR_BTN.disabled = false;
+  TABLERO_CTX.fillStyle = 'red';
+  TABLERO_CTX.font = 'bold 24px Arial';
+  TABLERO_CTX.fillText('¡Perdiste!', 45, 105);
+}
+
+function reiniciar() {
+  inicializar();
+  REINICIAR_BTN.disabled = true;
+  PUNTAJE_TXT.innerText = puntaje;
+}
+
 function main() {
-  if (perdioJuego()) return;
+  if (perdioJuego()) {
+    terminarJuego();
+    return;
+  };
   setTimeout(function() {
     limpiarTablero();
     moverCulebra();
@@ -136,7 +171,6 @@ function main() {
 }
 
 document.addEventListener('keydown', cambiarDireccion);
+REINICIAR_BTN.addEventListener('click', reiniciar);
 
-crearComida();
-
-main();
+inicializar();
